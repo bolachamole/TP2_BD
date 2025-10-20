6a8ad93050ed84e7040ce5b1073438db32776bcc
lilian-as <lilian.alvesant@gmail.com>
2025-10-19 21:11:29 -0400

    novos gerenciaBlocos

---
+++ include/GerenciaBlocos.hpp
@@ -0,0 +1,24 @@
+#ifndef GERENCIA_BLOCO
+#define GERENCIA_BLOCO
+
+#include <fstream>//biblioteca para manipulacao de arquivos
+#include <string>
+
+class GerenciaBlocos{
+	private:
+		const char* arq_entrada;
+		unsigned int size_blocos;
+
+	public:
+		GerenciaBlocos(std::string arq_nome);
+
+		void setSize_blocos(int fd);
+		unsigned int getSize_blocos();
+
+		static std::fstream abreArquivo(const std::string &nome_arquivo, std::ios::openmode modo);
+		static void fechaArquivo(std::fstream &arquivo);
+		static void lerBloco(std::fstream &arquivo,int indice, char* buffer);
+		static int totalDeBlocos(std::fstream &arquivo, unsigned int size_blocos);
+};
+
+#endif
---
+++ src/GerenciaBlocos.cpp
@@ -0,0 +1,47 @@
+#include <sys/stat.h>
+#include <iostream>
+#include "../include/GerenciaBlocos.hpp"
+
+GerenciaBlocos::GerenciaBlocos(std::string arq_nome){
+	this->arq_entrada = arq_nome.c_str();
+}
+
+void GerenciaBlocos::setSize_blocos(int fd){
+	struct stat infostat;
+	unsigned int tam_bloco;
+
+	if(lstat(this->arq_entrada,&infostat) == -1){
+		std::cout << "Erro ao tentar pegar o tamanho dos blocos.";
+		exit(1);
+	}
+	tam_bloco = (unsigned int)infostat.st_blksize;
+	this->size_blocos = tam_bloco;
+}
+
+unsigned int GerenciaBlocos::getSize_blocos(){
+	return this->size_blocos;
+}
+
+//abre arquivo
+std::fstream GerenciaBlocos::abreArquivo(const std::string &nome_arquivo, std::ios::openmode modo){
+	std::fstream arquivo(nome_arquivo, modo | std::ios::binary);
+	if(!arquivo.is_open()){ //verifica se o arquivo foi aberto
+		std::cerr << "Erro ao abrir o arquivo: " << nome_arquivo << std::endl;
+		exit(1);
+	}
+	return arquivo;
+}
+void GerenciaBlocos::fechaArquivo(std::fstream &arquivo){ //fecha arquivo
+	arquivo.close();
+}
+
+int GerenciaBlocos::totalDeBlocos(std::fstream &arquivo){ //calcula total de blocos
+	arquivo.seekg(0, std::ios::end);
+	std::streampos tamanho_arquivo = arquivo.tellg();
+	return static_cast<int>(tamanho_arquivo) / this->size_blocos; //total de blocos de acorodo com o tamanho do arquivo e tamanho dos blocos
+}
+
+void GerenciaBlocos::lerBloco(std::fstream &arquivo, int indice, char* buffer){ //le bloco do arquivo
+	arquivo.seekg(indice * this->size_blocos, std::ios::beg); //posiciona o ponteiro no inicio do bloco
+	arquivo.read(buffer, this->size_blocos); //le o bloco e armazena no buffer
+}
